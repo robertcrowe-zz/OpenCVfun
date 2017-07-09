9e83a48c9e23cd5a4f0da570483a93c56e8bd0e3
rcrowe <rcrowe@boingo.com>
2017-07-08 20:09:30 -0700

    Tweaks

--- identify_shapes.py
+++ identify_shapes.py
@@ -18,17 +18,17 @@ ret, thresh = cv2.threshold(gray, 127, 255, 1) # threshold to improve contrast f
 _, contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
 for cnt in contours:
-    # Get approximate polygons
+    # Get approximate polygons - values are tuned to image
     approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt,True), True)
     
-    if len(approx) == 3:
+    if len(approx) == 3: # must be triangle
         shape_name = "Triangle"
         cv2.drawContours(image, [cnt], 0, (0,255,0), -1)
         M = cv2.moments(cnt) # Find contour center to place text at the center
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
         cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-    elif len(approx) == 4:
+    elif len(approx) == 4: # could be either square or rectangle
         x, y, w, h = cv2.boundingRect(cnt)
         M = cv2.moments(cnt) # Find contour center to place text at the center
         cx = int(M['m10'] / M['m00'])
@@ -47,14 +47,14 @@ for cnt in contours:
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
             cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-    elif len(approx) == 10:
+    elif len(approx) == 10: # for this example, only star has 10
         shape_name = "Star"
         cv2.drawContours(image, [cnt], 0, (255, 255, 0), -1)
         M = cv2.moments(cnt)
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
         cv2.putText(image, shape_name, (cx-50, cy), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1)
-    elif len(approx) >= 15:
+    elif len(approx) >= 15: # must be a circle
         shape_name = "Circle"
         cv2.drawContours(image, [cnt], 0, (0, 255, 255), -1)
         M = cv2.moments(cnt)
